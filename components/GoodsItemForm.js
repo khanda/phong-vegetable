5dda66b6570fb13f9e7fbe90d00b29a27fc3055f
doanquangquyen <doanquangquyen@gmail.com>
2018-10-18 17:37:18 +0700

    create bill form

--- components/GoodsItemForm.js
+++ components/GoodsItemForm.js
@@ -1,17 +1,73 @@
 import React from 'react'
-import {ScrollView, StyleSheet, View} from 'react-native'
-import {MonoText} from "./StyledText";
+import {Button, ScrollView, StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+import {Icon} from 'react-native-elements'
 
 const GoodsItemForm = ({goods}) => {
   return (
     <View style={styles.container}>
-      <MonoText>{goods.name}</MonoText>
+      <View style={styles.flexRowContainer}>
+        <Text style={styles.label}>{goods.name}</Text>
+        <Text style={styles.label}>{goods.quantity}</Text>
+      </View>
+      <View style={styles.flexRowContainer}>
+        <TouchableOpacity style={styles.plusBtn} onPress={() => console.log('hello')}>
+          <Icon style={styles.actionButton}
+                name='plus'
+                size={26}
+                type='font-awesome'
+                color='white'
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.minusBtn} onPress={() => console.log('hello')}>
+          <Icon style={styles.actionButton}
+                name='minus'
+                size={26}
+                type='font-awesome'
+                color='white'
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    padding: 10
+  },
+  flexRowContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 17
+  },
+  actionButton: {
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  plusBtn: {
+    backgroundColor: 'green',
+    padding: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius:5
+  },
+  minusBtn: {
+    backgroundColor: 'red',
+    padding: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius:5
+  }
+
 });
 
 export default GoodsItemForm
