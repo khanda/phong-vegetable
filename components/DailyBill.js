d8be4e3cbee0f6a0188710b2ed2977b55d86e2fd
doanquangquyen <doanquangquyen@gmail.com>
2018-10-22 11:52:11 +0700

    chang bill list items style

--- components/DailyBill.js
+++ components/DailyBill.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import {StyleSheet, View, ScrollView} from 'react-native'
-import {List, ListItem} from 'react-native-elements'
+import {Container, Header, Content, List, ListItem, Text, Left, Right, Body, Thumbnail} from 'native-base';
+
 
 const DailyBill = ({dailyBillItems}) => {
   return (
     <View style={styles.container}>
-      <List>
-        {
-          dailyBillItems.map((item) => (
-            <ListItem
-              key={item._id}
-              title={item.name}
-              subtitle={item.quantity}
-              roundAvatar
-              icon={{uri: item.icon}}
-              rightIcon={{name: 'chevron-right', color: 'transparent'}}
-            />
-          ))
-        }
+
+      <List dataArray={dailyBillItems}
+            renderRow={(item) =>
+              <ListItem avatar>
+                <Left>
+                  {item.icon &&
+                    <Thumbnail small source={{uri: item.icon}}/>
+                  }
+                </Left>
+                <Body>
+                <Text>{item.name}</Text>
+                {/*<Text note></Text>*/}
+                </Body>
+                <Right>
+                  <Text>{item.quantity}</Text>
+                </Right>
+              </ListItem>
+            }>
       </List>
     </View>
   )
